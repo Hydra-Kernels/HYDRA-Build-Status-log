a2657caf577983293e0671c917bf176920e23e23
Harshit Aggarwal <LecCmD@protonmail.com>
2022-12-20 19:22:54 +0530

    Create scroll-through.sh

---
+++ scroll-through.sh
@@ -0,0 +1,9 @@
+Link to HYDRA LEVEL 7.13 Tree : 
+
+https://github.com/HarshitX97/HYDRA-LvL-7.13.git -b ext
+https://github.com/HarshitX97/HYDRA-LvL-7.13.git
+
+Clang Used : https://gitlab.com/Panchajanya1999/azure-clang.git 
+My thoughts : Amit has already used a lot of DragonTc and Proton-Clang, but its time for the latest builded clang, azure clang, bitch lets tryyy 
+
+Any Kernel 3 : https://github.com/HarshitX97/AK3.git -b HYDRA
