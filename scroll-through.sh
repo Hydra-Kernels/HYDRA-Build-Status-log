6b1d6462d0d133724078ef11e301d2367fa11464
Harshit Aggarwal <LecCmD@protonmail.com>
2022-12-20 19:46:59 +0530

    Update scroll-through.sh

--- scroll-through.sh
+++ scroll-through.sh
@@ -4,6 +4,6 @@ https://github.com/HarshitX97/HYDRA-LvL-7.13.git -b ext
 https://github.com/HarshitX97/HYDRA-LvL-7.13.git
 
 Clang Used : https://gitlab.com/Panchajanya1999/azure-clang.git 
-My thoughts : Amit has already used a lot of DragonTc and Proton-Clang, but its time for the latest builded clang, azure clang, bitch lets tryyy 
+My thoughts : Amit has already used a lot of DragonTc and Proton-Clang, but its time for the latest builded clang, azure clang, bitxh lets tryyy 
 
 Any Kernel 3 : https://github.com/HarshitX97/AK3.git -b HYDRA
